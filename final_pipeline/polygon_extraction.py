097a7758a841d0132818a0aa90eba21b5ef519b1
jonathanconroy <jonathanconroy14@gmail.com>
2020-07-29 18:41:24 -0400

    Remove hardcoded contour detection

--- final_pipeline/polygon_extraction.py
+++ final_pipeline/polygon_extraction.py
@@ -25,22 +25,43 @@ def extract_polygon(input_filepath, output_filepath, contour_accuracy = 5):
     maxLabels, labels = cv2.connectedComponents(im_th2)
 
 
+    # We need to determine which label corresponds to the "background"
+    #   connected component. There may be noise in the input data, but this
+    #   should be the label present at all the edges of the image.
+    # Heuristic: select the label that appears most frequently at the edges
+    upper_edge_labels = labels[0, :]
+    lower_edge_labels = labels[-1, :]
+    left_edge_labels  = labels[:, 0]
+    right_edge_labels = labels[:, -1]
+    edge_labels = np.concatenate((upper_edge_labels, lower_edge_labels,
+                                 left_edge_labels, right_edge_labels))
+    background_label = np.bincount(edge_labels).argmax()
+
+
     room_outline = np.zeros_like(labels, dtype=np.uint8)
-    room_outline[labels == 1] = 1
+    room_outline[labels == background_label] = 1
+
+    # Find the contour corresponding to the room outline
+    # Ideally room_outline contains a single connected component (hence only
+    # outline), but in practice there is some noise.
+    # Heuristic: select the "longest" contour
+    #            (the contour with the most points)
     contours, _ = cv2.findContours(room_outline,
                                    cv2.RETR_TREE,
                                    cv2.CHAIN_APPROX_SIMPLE)
+    room_contour_idx = np.argmax([contour.shape[0] for contour in contours])
+    room_contour = contours[room_contour_idx]
 
 
-    # Problem 1: this is where we hardcode the solution
-    # We are picking boundary 15 because it is what works on this code
-    poly = cv2.approxPolyDP(contours[15], contour_accuracy, True)
+    # Find approximate polygon
+    poly = cv2.approxPolyDP(room_contour, contour_accuracy, True)
     p = poly.reshape(poly.shape[0], poly.shape[2])
     p = np.append(p, [p[0]], axis=0)
 
     fig, ax = plt.subplots(figsize=(4, 8))
     ax.imshow(raw)
     ax.plot(p[:, 0], p[:, 1], color='red', linewidth=4)
-    plt.savefig(output_filepath)
+    plt.show()
+    #plt.savefig(output_filepath)
 
     return(Polygon(p))
